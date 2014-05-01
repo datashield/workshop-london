03541615f672ee5e626185d37a25fbe01d7676f7
Yannick Marcon <yannick.marcon@obiba.org>
2014-04-30 21:18:48 -0400

    test updated

--- datashield-ecp-test.R
+++ datashield-ecp-test.R
@@ -9,8 +9,7 @@ myvar <- list("PM_HIP_SIZE","PM_WEIGHT")
 opals <- datashield.login(logins=logindata, assign=TRUE, variables=myvar)
 
 # do some analysis
-ds.class(opals,quote(D$PM_HIP_SIZE))
-ds.class(opals,quote(D$PM_WEIGHT))
+# ...
 
 # free server resources
 datashield.logout(opals)
